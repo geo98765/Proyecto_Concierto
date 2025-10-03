c7c14a2182197226fbe0cbc99fdf113b2d60ef56
geo98765 <146154971+geo98765@users.noreply.github.com>
2025-10-03 14:17:42 -0600

    Add files via upload

---
+++ rockStadium/src/test/java/com/example/rockStadium/RockStadiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rockStadium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockStadiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
